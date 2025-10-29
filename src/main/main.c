3b98344008048ff7db6078ff24dc716a42210173
Some_Guy <mydiscord9133@gmail.com>
2025-10-29 19:05:17 +0600

    Abstracted glfw initialization away into a single function in renderer/

--- src/main/main.c
+++ src/main/main.c
@@ -12,30 +12,6 @@ int main()
 {
 	glfwInit();
 
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-
-	GLFWwindow *window = glfwCreateWindow(800, 300, "my window", NULL, NULL);
-	glfwMakeContextCurrent(window);
-
-	if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
-	{
-		printf("GLAD COULD NOT LOAD");
-		return -1;
-	}
-
-	glfwSetFramebufferSizeCallback(window, framebuf_callback);
-
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	while(!glfwWindowShouldClose(window))
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	
 	glfwTerminate();
 	return 0;
 }
